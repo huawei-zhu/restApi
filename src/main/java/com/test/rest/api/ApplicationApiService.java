be9e865971559ebde10f0135f7bdc4c26d23989e
test <test@officedepot.com>
2015-04-08 10:12:42 +0800

    test

---
+++ src/main/java/com/test/rest/api/ApplicationApiService.java
@@ -0,0 +1,50 @@
+package com.test.rest.api;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.test.rest.api.model.User;
+
+
+@Path("/api")
+public class ApplicationApiService {
+
+	@GET
+	@Path("/get")
+	@Produces(MediaType.APPLICATION_JSON)
+	public User getTrackInJSON2() {
+
+		User instance = new User();
+		instance.setName("test");
+
+		return instance;
+
+	}
+
+
+	@GET
+	@Path("/get")
+	@Produces({ MediaType.TEXT_PLAIN,MediaType.APPLICATION_XML})
+	public String getTrackInJSON() {
+
+		return "track";
+
+	}
+
+	
+//	@POST
+//	@Path("/post")
+//	@Consumes(MediaType.APPLICATION_JSON)
+//	public Response createTrackInJSON(User track) {
+//
+//		String result = "Track saved : " + track;
+//		return Response.status(201).entity(result).build();
+//		
+//	}
+//	
+}
---
+++ src/main/java/com/test/rest/api/model/User.java
@@ -0,0 +1,115 @@
+package com.test.rest.api.model;
+
+public class User {
+	private String username;
+	private String password;
+
+	private String name;
+	private int age;
+	private boolean isMale;
+	private boolean isSocialAccount;
+
+	@Override
+	public String toString() {
+		return "User [username=" + username + ", password=" + password
+				+ ", name=" + name + ", age=" + age + ", isMale=" + isMale
+				+ ", isSocialAccount=" + isSocialAccount + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + age;
+		result = prime * result + (isMale ? 1231 : 1237);
+		result = prime * result + (isSocialAccount ? 1231 : 1237);
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result
+				+ ((password == null) ? 0 : password.hashCode());
+		result = prime * result
+				+ ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (age != other.age)
+			return false;
+		if (isMale != other.isMale)
+			return false;
+		if (isSocialAccount != other.isSocialAccount)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public boolean isMale() {
+		return isMale;
+	}
+
+	public void setMale(boolean isMale) {
+		this.isMale = isMale;
+	}
+
+	public boolean isSocialAccount() {
+		return isSocialAccount;
+	}
+
+	public void setSocialAccount(boolean isSocialAccount) {
+		this.isSocialAccount = isSocialAccount;
+	}
+
+}
