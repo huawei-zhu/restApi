66eba0063c14600c6fc2507e831992f248d982e6
lf <test@officedepot.com>
2015-04-08 10:39:09 +0800

    commit only

--- src/main/java/com/test/rest/api/ApplicationApiService.java
+++ src/main/java/com/test/rest/api/ApplicationApiService.java
@@ -1,12 +1,9 @@
 package com.test.rest.api;
 
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 import com.test.rest.api.model.User;
 
